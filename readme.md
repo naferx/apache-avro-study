e4edc38c8a295449aa920e905c8fa59cf6029af9
naferx <nafr.snabr@gmail.com>
2023-12-04 18:53:08 -0500

    more doc

--- readme.md
+++ readme.md
@@ -2,6 +2,13 @@
 
 Data serialization system
 
+#### Fundamentals
+
+##### Schema
+JSON file that defines the structure of the data 
+
+
+https://avro.apache.org/docs/1.11.1/specification/
 
 
 https://avro.apache.org/
@@ -14,5 +21,11 @@ user.avsc .
 Input files to compile:
   schemas/user.avsc
 ```
+
+java -jar lib/avro-tools-1.11.3.jar getschema input/output4.avro
+
+java -jar lib/avro-tools-1.11.3.jar fromjson input/sample1.json --schema-file input/schema1.avsc > input/out.avro
+
+ java -jar lib/avro-tools-1.11.3.jar tojson input/out.avro
 
  `mvn -q exec:java -Dexec.mainClass=example.avro.Main`
--- src/main/java/example/avro/Main.java
+++ src/main/java/example/avro/Main.java
@@ -3,20 +3,21 @@ package example.avro;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumWriter;
+import java.util.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         User user1 = User.newBuilder()
-        .setName("Charlie")
-        .setFavoriteColor("blue")
-        .setFavoriteNumber(null)
-        .build();  
-        
-        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
-DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
-dataFileWriter.create(user1.getSchema(), new java.io.File("users.avro"));
-dataFileWriter.append(user1);
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber("22")
+                .build();
 
-dataFileWriter.close();
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+        dataFileWriter.append(user1);
+
+        dataFileWriter.close();
     }
 }
